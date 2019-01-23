a2bbe12caf425e07d0b24566b3e448f009b4aac1
hasadi-ha <hasanonomy@gmail.com>
2019-01-23 16:42:02 -0500

    Function made for checking if IP active or not

--- Scanner.py
+++ Scanner.py
@@ -1,1 +1,21 @@
 # Let It Begin!
+
+import time
+import logging
+from scapy.all import *
+
+target = input('Enter host to be scanned: ')
+
+print('Starting scan on host: ')
+
+closed_ports = 0
+open_ports = []
+
+
+def check_IP_active(target):
+    icmp = IP(dst=target)/ICMP()
+    resp = sr1(icmp, timeout=10)
+    if resp == None:
+        return false
+    else:
+        return True
